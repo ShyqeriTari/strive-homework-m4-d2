a54228974d39a1a5328be8dfe624d43510187096
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-02-08 15:28:07 +0100

    Changed text and background color of Jumbotrone

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,10 +2,10 @@ import { Jumbotron, Container, h1, p } from "react-bootstrap";
 
 const Welcome = (props) => (
 
-<Jumbotron fluid>
+<Jumbotron fluid className="bg-info">
   <Container>
-    <h1>{props.branding}</h1>
-    <p>
+    <h1 className='text-white'>{props.branding}</h1>
+    <p className='text-white'>
    {props.branding2}
     </p>
   </Container>
